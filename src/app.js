2df7ef5fd0a4b771d6f0869b5ab520c6d5bc03c7
nachodev <159072179+nachodev30@users.noreply.github.com>
2024-12-16 20:14:11 +0000

    Random-Card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palo = ["Diamantes", "Picas", "Corazones", "Treboles"];
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  let randomPaloNumber = Math.floor(Math.random() * palo.length);
+  let finalPalo = palo[randomPaloNumber];
+
+  document.getElementById("cardContent").innerHTML =
+    cardNumber[randomCardNumber];
+  document.getElementById("Card").className = "";
+  document.getElementById("Card").classList.add("card");
+  document.getElementById("Card").classList.add(getPalo(finalPalo));
 };
+
+function getPalo(palo) {
+  switch (palo) {
+    case "Diamantes":
+      return "card-diamantes";
+    case "Picas":
+      return "card-picas";
+    case "Corazones":
+      return "card-corazones";
+    case "Treboles":
+      return "card-treboles";
+  }
+}
